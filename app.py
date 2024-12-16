3cc48f0911e8711daccb3a3935c4ada9c24260df
SimonyiDaniel <simonyi.daniel.gyorgy@diak.szbi-pg.hu>
2024-12-16 08:46:38 +0100

    start

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from country_list import get_countries
+import random
 
 countries = get_countries()
 print(countries)
+
+country = random.choice(countries)
+
+letters = list(country)
+
+print("Welcome to the Hangman game!")
+print(f"The word is {len(country)} long.")
+print("Guess a letter!")
+print("_ " * len(letters))
+

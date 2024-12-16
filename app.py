3a50093e1c9e04a47a48e1df0632a8527713a928
SimonyiDaniel <simonyi.daniel.gyorgy@diak.szbi-pg.hu>
2024-12-16 09:50:40 +0100

    updateing

--- app.py
+++ app.py
@@ -6,34 +6,23 @@ country = random.choice(countries)
 print(country)
 letters = list(country)
 
-
-
 lives = 7
 
 print("Welcome to the Hangman game!")
 print('You have 7 lives, try to guess the country without losing them')
 print(f"The word is {len(country)} long.")
 print(guesses)
-while True:
-    guessed_letter = str(input("Guess a letter!"))
 
-    #if guessed_letter.lower() in letters:
-        #index = letters.index(guessed_letter)
-        #guesses[index] = guessed_letter
-        #print(guesses)
-    #else:
-        #print("kaki")
-
-    
-for index, guessed_letter in enumerate(letters):
-        guesses[index] = guessed_letter
-
-print(guesses)  
 
 for i in range(len(letters)):
-    print(guesses)
+    print("_ " * len(letters))
+    hits = []
     letter_input = str(input("Guess a letter!: "))
-    if letter_input in letters
+    if letter_input in letters and letter_input not in hits:
+        hits.append(letter_input)
+    elif: letter
+        
+        
     
     
 # for char in letters:
